8ce3784595e774e885f1ced49363a2613d5ea60c
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-06 13:52:52 +0400

    Update ArrayDequeCreator.java

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,19 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> sumOfQue = new ArrayDeque<>();
+        sumOfQue.add(firstQueue.poll());
+        sumOfQue.add(firstQueue.poll());
+        sumOfQue.add(secondQueue.poll());
+        sumOfQue.add(secondQueue.poll());
+        while (firstQueue.size() > 0 || secondQueue.size() > 0){
+            firstQueue.add(sumOfQue.poll());
+            sumOfQue.add(firstQueue.poll());
+            sumOfQue.add(firstQueue.poll());
+            secondQueue.add(sumOfQue.poll());
+            sumOfQue.add(secondQueue.poll());
+            sumOfQue.add(secondQueue.poll());
+        }
+        return sumOfQue;
     }
 }
