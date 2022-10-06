d796fa1c7bb778112cd79e58c65c6b7e8f0d23d2
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-06 13:53:19 +0400

    Update DishOrderDeterminer.java

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,42 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishList = new ArrayList<>();
+        //List<Integer> dishList = new ArrayList<>();
+//        for (int i = 1; i < numberOfDishes+1; i++) {
+//            dishList.add(i);
+//        }
+        if(numberOfDishes > 0){
+            dishList.add(everyDishNumberToEat);
+        }
+        int previous = everyDishNumberToEat;
+        for (int i = 1; i < numberOfDishes+1; i++) {
+            if(previous + everyDishNumberToEat < numberOfDishes+1){
+                if(!dishList.contains(previous + everyDishNumberToEat)){
+                    dishList.add(previous+everyDishNumberToEat);
+                    previous += everyDishNumberToEat;
+                }else {
+                    everyDishNumberToEat += 1;
+                    dishList.add(previous+everyDishNumberToEat);
+                    previous += everyDishNumberToEat;
+                }
+            } else if(previous + everyDishNumberToEat >= numberOfDishes){
+                if(!dishList.contains(previous + everyDishNumberToEat - numberOfDishes)){
+                    dishList.add(previous + everyDishNumberToEat - numberOfDishes);
+                    previous = previous + everyDishNumberToEat - numberOfDishes;
+                }else {
+                    everyDishNumberToEat += 1;
+                    dishList.add(previous + everyDishNumberToEat - numberOfDishes);
+                    previous = previous + everyDishNumberToEat - numberOfDishes;
+                }
+                everyDishNumberToEat += 1;
+            }
+        }
+
+        return dishList;
     }
 }
